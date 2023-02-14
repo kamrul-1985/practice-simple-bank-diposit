f0101839e55e8ec5c1f389be083b3eaa9a67edbf
Md Kamrul Islam <121995021+kamrul-1985@users.noreply.github.com>
2023-02-14 20:38:51 +0600

    edited

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.getElementById('deposit-button').addEventListener('click',function(){
     const inputeDipo = document.getElementById('input-deposit');
     const inputeDiposit = inputeDipo.value;
+
    const previousDipo = document.getElementById('previous-dipo');
   const previousDiposit = previousDipo.innerText;
+
    const totalDipositAmount = parseFloat(inputeDiposit) + parseFloat(previousDiposit);
    previousDipo.innerText = totalDipositAmount;
    inputeDipo.value ='';
@@ -17,6 +19,11 @@ document.getElementById('deposit-button').addEventListener('click',function(){
     const inputeWidth = document.getElementById('inpute-widthdro');
     const inputewidthdrow = inputeWidth.value;
 
+    const previouswidth = document.getElementById('previous-widthdro');
+    const previouswidthdrow = previouswidth.innerText;
+    const totalwidthdrow = parseFloat(inputewidthdrow) + parseFloat(previouswidthdrow);
+    previouswidth.innerText = totalwidthdrow;
+    
     const previousBale = document.getElementById('previous-balance');
   const previousBalence = previousBale.innerText;
   const totalBalance2 = parseFloat(previousBalence) - parseFloat(inputewidthdrow);
